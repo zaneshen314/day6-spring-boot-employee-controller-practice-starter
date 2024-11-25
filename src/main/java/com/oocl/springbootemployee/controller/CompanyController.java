830d02a17c48ff8fe3c48a2105880fc6a7ad6cf2
SHENZA <shenza@oocl.com>
2024-11-25 19:52:22 +0800

    [spring]feat: design Company api

--- src/main/java/com/oocl/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/oocl/springbootemployee/controller/CompanyController.java
@@ -2,6 +2,8 @@ package com.oocl.springbootemployee.controller;
 
 
 import com.oocl.springbootemployee.entity.Company;
+import com.oocl.springbootemployee.entity.CompanyResponse;
+import com.oocl.springbootemployee.entity.Employee;
 import com.oocl.springbootemployee.repository.CompanyRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +16,6 @@ public class CompanyController {
 
     private final CompanyRepository companyRepository;
 
-
     public CompanyController(CompanyRepository companyRepository) {
         this.companyRepository = companyRepository;
     }
@@ -23,4 +24,38 @@ public class CompanyController {
     public List<Company> getAllCompanies() {
         return companyRepository.getAll();
     }
+
+    @GetMapping("/{id}")
+    public CompanyResponse getCompanyById(@PathVariable Long id) {
+        return null;
+    }
+
+    @GetMapping("/{id}/employees")
+    public List<Employee> getEmployeesByCompanyId(@PathVariable Long id) {
+        return null;
+    }
+
+    @GetMapping(params = {"page", "size"})
+    public List<Company> getCompaniesByPage(@RequestParam Integer page, @RequestParam Integer size) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public void updateCompany(@PathVariable Long id, @RequestBody Company company) {
+
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Company createCompany(@RequestBody Company company) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCompany(@PathVariable Long id) {
+
+    }
+
+
 }
---
+++ src/main/java/com/oocl/springbootemployee/entity/CompanyResponse.java
@@ -0,0 +1,8 @@
+package com.oocl.springbootemployee.entity;
+
+
+public class CompanyResponse {
+
+    private Long id;
+    private String name;
+}
